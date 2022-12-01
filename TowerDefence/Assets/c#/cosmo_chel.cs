a138019f532b03dbf316440fad2a2224cb443914
Sergey <spanteleev670@gmail.com>
2022-12-02 01:18:07 +0300

    Смена положения и стрельба + ошибка
    Ошибка, которая возникала из-за замены prefab решена.
    Разобрался, как создать и грамотно прикрепить физическое место возникновения пуль (персонаж идет влево - пули тоже будут корректно лететь влево, вылетают ровно из рук, моделька отзеркаливается, камера и свет следуют всегда за плейером)

--- TowerDefence/Assets/c#/cosmo_chel.cs
+++ TowerDefence/Assets/c#/cosmo_chel.cs
@@ -7,10 +7,13 @@ public class cosmo_chel : MonoBehaviour
     public float speed = 10;
     public float xAxis, yAxis;
     public Transform chel;
+    private bool FacingRight = true;
+    public GameObject bullets;
 
     void Start()
     {
-        chel = GetComponent<Transform>();    
+        chel = GetComponent<Transform>();
+        bullets = GameObject.FindWithTag("bullets");
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -26,5 +29,25 @@ public class cosmo_chel : MonoBehaviour
         yAxis = Input.GetAxis("Vertical");
         Vector3 moveVector = new Vector3(xAxis, yAxis, 0);
         chel.Translate(moveVector * speed * Time.deltaTime);
+
+        if(xAxis > 0 && FacingRight)
+        {
+            Flip();
+        }
+        else if(xAxis < 0 && !FacingRight)
+        {
+            Flip();
+        }
+    }
+
+    private void Flip()
+    {
+        
+        FacingRight = !FacingRight; 
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+        bullets.transform.Rotate(0f, 180f, 0f);
+
     }
 }
