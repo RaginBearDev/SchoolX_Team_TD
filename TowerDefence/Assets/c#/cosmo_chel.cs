512e9bb068e4a329a3212a34b8bb9bba4b8f3c4c
Sergey <spanteleev670@gmail.com>
2022-12-01 16:54:15 +0300

    Первая сцена
    Загрузил в проект объекты картинками, космонавт двигается, ударяется об стенки-рамки

---
+++ TowerDefence/Assets/c#/cosmo_chel.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cosmo_chel : MonoBehaviour
+{
+    public float speed = 10;
+    public float xAxis, yAxis;
+    public Transform chel;
+
+    void Start()
+    {
+        chel = GetComponent<Transform>();    
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "platform")
+        {
+            Debug.Log("Hi");
+        }
+    }
+
+    void Update()
+    {
+        xAxis = Input.GetAxis("Horizontal");
+        yAxis = Input.GetAxis("Vertical");
+        Vector3 moveVector = new Vector3(xAxis, yAxis, 0);
+        chel.Translate(moveVector * speed * Time.deltaTime);
+    }
+}
